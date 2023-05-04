8edd76925b55b503532bb7b6718b9e57c04cb3a2
Dushan Tharusha <thaurushanelligahawaththa@gmail.com>
2023-05-04 10:02:57 +0530

    test 1

---
+++ app/src/main/java/com/example/mad_money_management/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mad_money_management
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+          //donaldnlas
+        setContentView(R.layout.aboutus)
+    }
+}
